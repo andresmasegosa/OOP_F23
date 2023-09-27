e1b31dcf18821c76a6f4380f7987afd3c3ac0fd3
Andres Arredondo <andresmasegosa@gmail.com>
2023-09-27 13:32:51 +0200

    Update

--- src/session3/B_DesignWithInheritance/ChessDemo.java
+++ src/session3/B_DesignWithInheritance/ChessDemo.java
@@ -7,6 +7,23 @@ public class ChessDemo {
         ChessBoard chessBoard = new ChessBoard();
 
 
+        ChessPiece blackKing = new King(chessBoard,"Black", 0, 2);
+
+        chessBoard.moveChessPiece(blackKing,1,2);
+
+
+
+
+        //blackKing.isChecked();
+
+
+        Queen blackQueen = new Queen(chessBoard,"Black", 0, 1);
+        blackQueen.isLegalTheMovement(0,6);
+
+        chessBoard.moveChessPiece(blackQueen,1,2);
+
+
+
         //Start to move pieces
         //boolean success = chessBoard.moveChessPiece(queen,0,1);
         //System.out.println(success);
